6d0e1ddd2a0c83e70e562df7c0cd016d2141dd6d
jimmy <jimmy.yang@moprobo.com>
2023-09-19 15:14:05 +0800

    fix the cmakelist
    Signed-off-by: jimmy <jimmy.yang@moprobo.com>

--- test_rk3588_yolov5.sh
+++ test_rk3588_yolov5.sh
@@ -4,5 +4,5 @@ ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
 INSTALL_DIR=${ROOT_PWD}/install
 
 cd ${INSTALL_DIR}
-./rk3588_yolov5 assets/yolov5s_logo_quan_0918.rknn assets/logo.jpg
+./quickrun assets/yolov5s_logo_quan_0918.rknn assets/logo.jpg
 cd -
